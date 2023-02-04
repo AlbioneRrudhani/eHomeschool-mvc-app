9517cac18fdd185482e087505cd2aa51a03a28ed
Albioneee <ar52638@ubt-uni.net>
2023-02-04 01:24:26 +0100

    Added shopping cart model.

---
+++ eHomeschool/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,32 @@
+﻿using eHomeschool.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eHomeschool.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+
+         
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems
+                .Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Course).ToList());
+        }
+
+        public double GetShoppingCartTotal() => _context.ShoppingCartItems
+            .Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Course.Price * n.Amount).Sum();
+    }
+}
