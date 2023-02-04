a8ec94d7072bd834e9a82cf187fb0be5112d900c
Albioneee <ar52638@ubt-uni.net>
2023-02-04 22:26:51 +0100

    Added identity.

---
+++ eHomeschool/Migrations/20230204210112_Initial-updated.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eHomeschool.Migrations
+{
+    public partial class Initialupdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
