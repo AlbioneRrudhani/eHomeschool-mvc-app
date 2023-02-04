6b72aa9c5928d40a36b87fc08c69c6ef3c73b847
Albioneee <ar52638@ubt-uni.net>
2023-02-04 01:56:07 +0100

    Added orders controller and shopping cart view.

---
+++ eHomeschool/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using eHomeschool.Data.Cart;
+using eHomeschool.Data.Service;
+using eHomeschool.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eHomeschool.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ICourseService _courseService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(ICourseService courseService, ShoppingCart shoppingCart)
+        {
+            _courseService = courseService;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
---
+++ eHomeschool/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using eHomeschool.Data.Cart;
+
+namespace eHomeschool.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
