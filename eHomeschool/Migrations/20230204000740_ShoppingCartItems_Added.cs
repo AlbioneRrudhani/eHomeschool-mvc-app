d7643941e609931a3de0cb1f10e13d4b7096a51d
Albioneee <ar52638@ubt-uni.net>
2023-02-04 01:12:46 +0100

    Added ShoppingCartItems model.

---
+++ eHomeschool/Migrations/20230204000740_ShoppingCartItems_Added.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eHomeschool.Migrations
+{
+    public partial class ShoppingCartItems_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingCartItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseId = table.Column<int>(type: "int", nullable: true),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    ShoppingCartId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCartItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCartItems_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCartItems_CourseId",
+                table: "ShoppingCartItems",
+                column: "CourseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingCartItems");
+        }
+    }
+}
---
+++ eHomeschool/Models/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eHomeschool.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Course Course { get; set; }
+        public int Amount { get; set; } //amount of courses
+
+
+        public string ShoppingCartId { get; set; }
+    }
+}
