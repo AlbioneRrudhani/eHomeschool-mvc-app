0cf18d430e55ff1b424f0ce1ae3370e30f905d61
Albioneee <ar52638@ubt-uni.net>
2023-02-05 01:56:31 +0100

    Complete registration process.

--- eHomeschool/Controllers/AccountController.cs
+++ eHomeschool/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using eHomeschool.Data;
+using eHomeschool.Data.Static;
 using eHomeschool.Data.ViewModels;
 using eHomeschool.Models;
 using Microsoft.AspNetCore.Identity;
@@ -46,6 +47,35 @@ namespace eHomeschool.Controllers
 
             TempData["Error"] = "Wrong credentials. Please, try again!";
             return View(loginVM);
+        }
+
+
+        public IActionResult Register() => View(new RegisterVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
         }
 
 
